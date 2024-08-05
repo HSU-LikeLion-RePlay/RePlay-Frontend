01342c53bd179587f2800d26a71cd037e5ac3821
“minnji-kim” <ohmygmg@naver.com>
2024-08-06 04:18:13 +0900

    playdetail

--- src/Play/component/PlayDetail.jsx
+++ src/Play/component/PlayDetail.jsx
@@ -3,8 +3,8 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { playInfoState, reviewState, userState, playscarapState } from '../../atoms';
 import '../style/PlayDetail.css';
-import bookmarkIcon from "../images/bookmark.jpg";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import bookmarkIcon from "../images/bookmark.jpg";
 import Comment from './Comment';
 import locimg from '../images/Loc.svg';
 import ConfirmModal from './ConfirmModal';
@@ -24,50 +24,21 @@ export default function PlayDetail() {
   const [isParticipating, setIsParticipating] = useState(false); // 참가 상태 확인
   const [showConfirmModal, setShowConfirmModal] = useState(false); // 참가 취소 모달 가시성 상태
   const mapRef = useRef();
-  const { index: playingId } = useParams();
-  const [post, setPost] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  const isScrapped = scraps.includes(playInfo?.id); // 해당 모임이 스크랩된 상태인지 확인
+  const isScrapped = scraps.includes(playInfo?.id); // 스크랩 상태 확인
 
   useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await fetch(
-          `https://43.201.176.194.nip.io/api/playing/getPlaying/${playingId}`,
-          {
-            method: "GET",
-            headers: {
-            },
-          }
-        );
-
-        if (response.status === 404) {
-          console.error("해당 글을 찾을 수 없습니다.");
-          setLoading(false);
-          return;
+    if (playInfo) {
+      const geocoder = new kakao.maps.services.Geocoder();
+      const coord = new kakao.maps.LatLng(playInfo.latitude, playInfo.longitude);
+      const callback = function (result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+          setAddress(result[0].address);
         }
-
-        const result = await response.json();
-
-        console.log("Response status:", response.status);
-        console.log("Response result:", result);
-
-        if (response.status === 200) {
-          setPost(result.data);
-          console.log("데이터 가져오기 성공:", result.data);
-        } else {
-          console.error(result.message);
-        }
-      } catch (error) {
-        console.error("네트워크 오류가 발생했습니다.", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchPost();
-  }, [playingId]);
+      };
+      geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+    }
+  }, [playInfo]);
 
   useEffect(() => {
     // 사용자가 현재 모임에 참가 중인지 확인하는 로직 추가
@@ -164,7 +135,7 @@ export default function PlayDetail() {
               <p>후기가 없습니다.</p>
             )}
           </div>
-          <hr/>
+          <hr />
           <Comment />
         </div>
         <div className='playdetail-right'>
